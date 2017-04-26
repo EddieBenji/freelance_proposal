0182cce8843b95b9fdbb9abc498aee05a1fbbc26
Lalo <eduardo_benji@hotmail.com>
2017-04-25 23:28:42 -0500

    proposal scaffold created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |proposal|
+  Proposal.create!(customer: "Customer #{proposal}",
+                   portfolio_url: (proposal % 2 == 0 ? 'http://portfolio.jordanhudgens.com/' : 'https://github.com/EddieBenji'),
+                   tools: 'RoR, Angular and Postgresql',
+                   estimated_hours: proposal*4,
+                   hourly_rate: 120,
+                   weeks_to_complete: proposal*2,
+                   client_email: (proposal % 2 == 0 ? 'eduardo_benji@hotmail.com' : 'eduardo.942712@gmail.com'))
+end
